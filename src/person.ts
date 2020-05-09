dfe5d4ec41423e38113b8f6fca0c1b4883a8ce54
Mahesh Haldar <haldar.mahesh@dkatalis.com>
2020-05-09 18:26:36 +0530

    Update examples

--- src/person.ts
+++ src/person.ts
@@ -1,3 +1,25 @@
+interface LengthConstraint {
+  length: number;
+}
+function getLength<T extends LengthConstraint>(data: T) {
+  return data.length;
+}
+
+
+
+getLength(66666666);
+
+getLength("SOMESTRING");
+
+getLength([34, 45]);
+
+
+
+
+
+
+
+
 import { convertToValueArray } from "./utils";
 
 export interface Person {
@@ -5,7 +27,7 @@ export interface Person {
 }
 
 const person: Person = {
-  name: "Mahesh"
+  name: "Mahesh",
 };
 
 const firstPerson = convertToValueArray(person)[0];
@@ -20,3 +42,72 @@ export const isValidString = (value: String): Boolean => {
 export const isValidArrayOfPerson = (value: Array<Person>): Boolean => {
   return value !== undefined && value.length !== 0;
 };
+
+function doStuff<T, U>(name: T): T {
+  // ...some process
+  return name;
+}
+interface DataBaseConnector {
+  get: Function;
+  put: Function;
+}
+abstract class BaseLocalDataBase<T, M> {
+  tableName: String;
+  dataBaseInstance: DataBaseConnector;
+  constructor(tableName: String) {
+    this.tableName = tableName;
+    this.dataBaseInstance = getDatabase<T>(tableName);
+  }
+  async insert(data: M): Promise<void> {
+    await this.dataBaseInstance.put(data);
+  }
+
+  async get(id: Number): Promise<M> {
+    return await this.dataBaseInstance.get(id);
+  }
+
+  abstract getFormattedData(): Array<M>;
+}
+interface ContactTable {
+  name: String;
+}
+interface ContactModel {
+  id: String;
+  name: String;
+  phoneNumber: String;
+}
+class ContactLocalDatabase<T> extends BaseLocalDataBase<
+  ContactTable,
+  ContactModel
+> {
+  getFormattedData(): ContactModel[] {
+    // format and return data
+  }
+}
+
+let contactsLocalDatabase = new ContactLocalDatabase("Contact table");
+
+await contactsLocalDatabase.get(21);
+
+
+const contactModel: ContactTable = {
+  id: 12,
+  name: 'Some name',
+
+  ...,            // define all other values.
+  ...
+}
+await contactsLocalDatabase.insert(contactModel);
+
+const contactArray: Array<ContactModel> = contactsLocalDatabase.getFormattedData();
+
+
+
+// class HttpClient extends BaseHttpClient<String, Boolean> {
+//   get(id: Number): String {
+//     throw new Error("Method not implemented.");
+//   }
+//   post(requestBody: String): Boolean {
+//     throw new Error("Method not implemented.");
+//   }
+// }
